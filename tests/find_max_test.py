0d3229b015b2076862601471569ccb8d2d94b641
Yernar Kambar <ernar.k.20@gmail.com>
2018-10-09 21:13:42 +0600

    I wrote 7 test functions in find_max_test.py

--- tests/find_max_test.py
+++ tests/find_max_test.py
@@ -1,24 +1,38 @@
 from unittest import TestCase
-
+import find_max as f
 
 class FindMaxTest(TestCase):
     def test_get_max(self):
-        pass
-
+        result = f.get_max(1, 70)
+        self.assertEqual(result, 70)
     def test_get_max_without_arguments(self):
-        pass
+        self.assertRaises(TypeError, f.get_max_without_arguments)
 
     def test_get_max_with_one_argument(self):
-        pass
+        result = f.get_max_with_one_argument(123)
+        self.assertEqual(result, 123)
 
     def test_get_max_with_many_arguments(self):
-        pass
+        result = f.get_max_with_many_arguments(1, 2, 3, 4)
+        self.assertEqual(4, result)
 
     def test_get_max_with_one_or_more_arguments(self):
-        pass
+        first = 124
+        array = [10000, 1421, 12]
+        result = f.get_max_with_one_or_more_arguments(first, *array)
+        self.assertEqual(10000, result)
 
     def test_get_max_bounded(self):
-        pass
+        kwargs = {
+            'low': 0,
+            'high': 127
+        }
+        result = f.get_max_bounded(-54, 45, 23, **kwargs)
+        self.assertEqual(45, result)
 
     def test_make_max(self):
-        pass
+        bounded_min = f.make_max(low=0, high=255)
+        self.assertEqual(True, callable(bounded_min))
+        if callable(bounded_min):
+            result = bounded_min(-5, 12, 13)
+            self.assertEqual(13, result)
